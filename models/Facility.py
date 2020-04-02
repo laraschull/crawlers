ccbe56fc1ac569a6c0254bbbcd5ebb6aab070201
tdonati <tdonati@bu.edu>
2020-04-02 09:42:49 -0700

    Search and looping thru different profiles works

--- models/Facility.py
+++ models/Facility.py
@@ -9,7 +9,6 @@ class Facility:
         self.generatedID = self.state + "_" + self.name.replace(" ", "_")
         return self.generatedID
 
-
     def getDict(self):
         return {"_id": self.getGeneratedID(),
                 "name": self.name,
