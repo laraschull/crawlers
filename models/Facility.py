357278b4664b3a7f242c7c444413363ca73fe5ba
tdonati <tdonati@bu.edu>
2020-04-07 14:04:28 -0700

    add iowa crawler

--- models/Facility.py
+++ models/Facility.py
@@ -9,8 +9,8 @@ class Facility:
         self.generatedID = self.state + "_" + self.name.replace(" ", "_")
         return self.generatedID
 
+
     def getDict(self):
         return {"_id": self.getGeneratedID(),
                 "name": self.name,
                 "state": self.state
-                }
